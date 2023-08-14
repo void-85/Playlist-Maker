9a616eaf552d828d086c8aa1e2f7cd04f0fe2287
void-85 <zGonzo@doctor.com>
2023-08-15 02:11:15 +0500

    #55 media player play...

--- app/src/main/java/com/example/playlistmaker/DateTimeUtils.kt
+++ app/src/main/java/com/example/playlistmaker/DateTimeUtils.kt
@@ -5,5 +5,5 @@ import java.util.Locale
 
 
 fun Long.millisToMinSec(): String{
-    return SimpleDateFormat("m:ss", Locale.getDefault() ).format(this)
+    return SimpleDateFormat("mm:ss", Locale.getDefault() ).format(this)
 }
