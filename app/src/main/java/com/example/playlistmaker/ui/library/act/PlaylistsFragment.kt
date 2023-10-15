00538cf4d6d18542cd86c3ad3b434480bc57b953
void-85 <zGonzo@doctor.com>
2023-10-16 01:46:34 +0500

    #95 s18 ...

---
+++ app/src/main/java/com/example/playlistmaker/ui/library/act/PlaylistsFragment.kt
@@ -0,0 +1,39 @@
+package com.example.playlistmaker.ui.library.act
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.playlistmaker.databinding.FragmentPlaylistsBinding
+
+
+class PlaylistsFragment : Fragment() {
+
+    companion object {
+
+        private const val TEXT_KEY = "text"
+
+        fun newInstance(text: String) = PlaylistsFragment().apply {
+            arguments = Bundle().apply {
+                putString(TEXT_KEY, text)
+            }
+        }
+    }
+
+    private lateinit var binding: FragmentPlaylistsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.playlistsFragText.text = requireArguments().getString(TEXT_KEY)
+    }
+
+}
