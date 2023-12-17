3974209cc3b42ea5fc99d0c66d2ca77839403e07
void-85 <zGonzo@doctor.com>
2023-12-17 23:12:56 +0500

    #138 (S#21) remove comments

--- app/src/main/java/com/example/playlistmaker/data/interLayerConverters/TrackConverters.kt
+++ app/src/main/java/com/example/playlistmaker/data/interLayerConverters/TrackConverters.kt
@@ -5,47 +5,6 @@ import com.example.playlistmaker.domain.entities.Track
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 
-
-/*fun TrackDTO.convert(): DBTrackEntity {
-    return DBTrackEntity(
-
-        this.trackId,
-
-        this.trackName ?: "",
-        this.artistName ?: "",
-        this.trackTimeMillis?.millisToMinSec() ?: "0:00",
-        this.artworkUrl100 ?: "",
-
-        this.collectionName ?: "",
-        this.releaseDate ?: "",
-        this.primaryGenreName ?: "",
-        this.country ?: "",
-
-        this.previewUrl ?: ""
-    )
-}*/
-
-/*fun DBTrackEntity.convert(): Track {
-    return Track(
-
-        this.trackId,
-
-        this.isFavorite,
-    
-        this.trackName,
-        this.artistName,
-        this.trackTime,
-        this.artworkUrl100,
-    
-        this.collectionName,
-        this.releaseDate,
-        this.primaryGenreName,
-        this.country,
-    
-        this.previewUrl
-    )
-}*/
-
 fun Track.convert(): DBTrackEntity {
     return DBTrackEntity(
 
